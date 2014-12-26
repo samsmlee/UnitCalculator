09efcf9aa55ae87e427ea8781e2012d79312ad01
samsmlee <samsmlee@gmail.com>
2014-12-26 15:34:17 -0800

    Wrote a unit test class for Number for a very limited set of test cases
    Write a unit test class for Number #14

--- app/src/androidTest/java/com/samsmlee/unitcalculator/Number/NumberTest.java
+++ app/src/androidTest/java/com/samsmlee/unitcalculator/Number/NumberTest.java
@@ -2,14 +2,21 @@ package com.samsmlee.unitcalculator.Number;
 
 import com.samsmlee.unitcalculator.Unit.Unit;
 import com.samsmlee.unitcalculator.Unit.UnitBase.UnitBase;
+import com.samsmlee.unitcalculator.Unit.UnitFactor;
 
 import junit.framework.TestCase;
+
+import java.text.DecimalFormat;
 
 public class NumberTest extends TestCase {
 
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Tests for public Number convert(Unit toUnit)
+    ///////////////////////////////////////////////////////////////////////////
+
     // Null argument case
-    public void testConvert_NullArgument() throws Exception {
+    public void testConvert_1Unit_NullArgument() throws Exception {
         Number n = new Number(UnitBase.METER, 1);
 
         try {
@@ -20,15 +27,83 @@ public class NumberTest extends TestCase {
         }
     }
 
-    // meter to yard case
-    public void testConvert_MeterToYard() throws  Exception {
+
+    // METER to YARD case
+    public void testConvert_1Unit_MeterToYard() throws  Exception {
+        Number n = new Number(UnitBase.METER, 1);
+
+        Unit toUnit = new Unit(UnitBase.YARD, UnitFactor.ONE);
+        Number converted = n.convert(toUnit);
+
+        DecimalFormat df = new DecimalFormat("0.00000");
+
+        String formatted = df.format(converted.getValue());
+
+        assertEquals(1.09361, Double.parseDouble(formatted));
+
+    }
+
+
+    // GRAM to POUND case
+    public void testConvert_1Unit_GramToPound() throws  Exception {
+        Number n = new Number(UnitBase.GRAM, 1000);
+
+        Unit toUnit = new Unit(UnitBase.POUND, UnitFactor.ONE);
+        Number converted = n.convert(toUnit);
+
+        DecimalFormat df = new DecimalFormat("0.00000");
+
+        String formatted = df.format(converted.getValue());
+
+        assertEquals(2.20462, Double.parseDouble(formatted));
+
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Tests for public Number convert(UnitBase toBase)
+    ///////////////////////////////////////////////////////////////////////////
+    // Null argument case
+    public void testConvert_1UnitBase_NullArgument() throws Exception {
+        Number n = new Number(UnitBase.METER, 1);
+
+        try {
+            n.convert((UnitBase)null);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
+
+    // METER to YARD case
+    public void testConvert_1UnitBase_MeterToYard() throws  Exception {
         Number n = new Number(UnitBase.METER, 1);
 
         Number converted = n.convert(UnitBase.YARD);
 
-        assertEquals(1.09361, converted.getValue());
+        DecimalFormat df = new DecimalFormat("0.00000");
+
+        String formatted = df.format(converted.getValue());
+
+        assertEquals(1.09361, Double.parseDouble(formatted));
 
     }
+
+    // GRAM to POUND case
+    public void testConvert_1UnitBase_GramToPound() throws  Exception {
+        Number n = new Number(UnitBase.GRAM, 1000);
+
+        Number converted = n.convert(UnitBase.POUND);
+
+        DecimalFormat df = new DecimalFormat("0.00000");
+
+        String formatted = df.format(converted.getValue());
+
+        assertEquals(2.20462, Double.parseDouble(formatted));
+
+    }
+
+
 
 
 }
