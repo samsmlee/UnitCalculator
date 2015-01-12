2598995e6b50a84460f5ecc0b896c0a1cd08f43b
samsmlee <samsmlee@gmail.com>
2015-01-11 23:26:30 -0800

    Add a unit test for the decimal point key #84

--- app/src/androidTest/java/com/samsmlee/unitcalculator/Calculator/DecimalPointKeyTest.java
+++ app/src/androidTest/java/com/samsmlee/unitcalculator/Calculator/DecimalPointKeyTest.java
@@ -48,6 +48,123 @@ public class DecimalPointKeyTest extends ActivityInstrumentationTestCase2<Calcul
 
     }
 
+    @UiThreadTest
+    public void testDecimalPointKey1() throws Exception {
+
+        // press Decimal Point right away, then "1"
+        pressKey(R.id.decimal_point_key);
+        pressKey(R.id.one_key);
+        assertEquals(" 0.1", fromScreenContent());
+        assertEquals("To", toScreenContent());
+
+    }
+
+    @UiThreadTest
+    public void testDecimalPointKey2() throws Exception {
+
+        // press Decimal Point after " 1234"
+        pressKey(R.id.one_key);
+        pressKey(R.id.two_key);
+        pressKey(R.id.three_key);
+        pressKey(R.id.four_key);
+        pressKey(R.id.decimal_point_key);
+        assertEquals(" 1234.", fromScreenContent());
+        assertEquals("To", toScreenContent());
+
+    }
+
+    @UiThreadTest
+    public void testDecimalPointKey3() throws Exception {
+
+        // press Decimal Point after " 1234", then enter "5"
+        pressKey(R.id.one_key);
+        pressKey(R.id.two_key);
+        pressKey(R.id.three_key);
+        pressKey(R.id.four_key);
+        pressKey(R.id.decimal_point_key);
+        pressKey(R.id.five_key);
+        assertEquals(" 1234.5", fromScreenContent());
+        assertEquals("To", toScreenContent());
+
+    }
+
+    @UiThreadTest
+    public void testDecimalPointKey4() throws Exception {
+
+        // press Decimal Point twice after " 1234"
+        pressKey(R.id.one_key);
+        pressKey(R.id.two_key);
+        pressKey(R.id.three_key);
+        pressKey(R.id.four_key);
+        pressKey(R.id.decimal_point_key);
+        pressKey(R.id.decimal_point_key);
+        assertEquals(" 1234.", fromScreenContent());
+        assertEquals("To", toScreenContent());
+
+    }
+
+    @UiThreadTest
+    public void testDecimalPointKey5() throws Exception {
+
+        // press Decimal Point after " 1234.5"
+        pressKey(R.id.one_key);
+        pressKey(R.id.two_key);
+        pressKey(R.id.three_key);
+        pressKey(R.id.four_key);
+        pressKey(R.id.decimal_point_key);
+        pressKey(R.id.five_key);
+        pressKey(R.id.decimal_point_key);
+        assertEquals(" 1234.5", fromScreenContent());
+        assertEquals("To", toScreenContent());
+
+    }
+
+
+    @UiThreadTest
+    public void testDecimalPointKey6() throws Exception {
+
+        // press Decimal Point after " 1234" and EQUAL key
+        pressKey(R.id.one_key);
+        pressKey(R.id.two_key);
+        pressKey(R.id.three_key);
+        pressKey(R.id.four_key);
+        pressKey(R.id.equal_key);
+        pressKey(R.id.decimal_point_key);
+        assertEquals(" 0.", fromScreenContent());
+        assertEquals("1234", toScreenContent());
+
+    }
+
+
+    @UiThreadTest
+    public void testDecimalPointKey7() throws Exception {
+
+        // press Decimal Point after " 1234.5" and EQUAL key
+        pressKey(R.id.one_key);
+        pressKey(R.id.two_key);
+        pressKey(R.id.three_key);
+        pressKey(R.id.four_key);
+        pressKey(R.id.decimal_point_key);
+        pressKey(R.id.five_key);
+        pressKey(R.id.equal_key);
+        pressKey(R.id.decimal_point_key);
+        assertEquals(" 0.", fromScreenContent());
+        assertEquals("1234.5", toScreenContent());
+
+    }
+
+    @UiThreadTest
+    public void testDecimalPointKey8() throws Exception {
+
+        // press Decimal Point after "-1"
+        pressKey(R.id.one_key);
+        pressKey(R.id.sign_key);
+        pressKey(R.id.decimal_point_key);
+        assertEquals("-1.", fromScreenContent());
+        assertEquals("To", toScreenContent());
+
+    }
+
     protected void pressKey(final int id) {
         mActivity.findViewById(id).performClick();
     }
